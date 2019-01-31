ec50a97c0562aa88408a09c24edd5b21d7fd6a27
lhn136 <32319480+lhn136@users.noreply.github.com>
2019-01-30 23:01:26 -0800

    Starting
    Importing things setting everything up

--- Magic.py
+++ Magic.py
@@ -3,7 +3,20 @@
 import random
 
 # Generate 2 random number in a list
-magic_number = [random.randomint(0,9),random.randint(0,9)]
+magic_numbers = [random.randomint(0,9),random.randint(0,9)]
 
 def user_input_conditional_check():
-    
+    user_number = int(input("Enter a number between 0 and 9: "))
+    if user_number in magic_numbers:
+        return "You got the number right!"
+    if user_number not in magic_numbers:
+        return "You didn't quite get it."
+
+def run_program_n_times(chances):
+    for attempt in range(chances):
+        print("This is attempt {}".format(attempt))
+        user_input_conditional_check()
+
+if __name__ == '__main__':
+    user_attempts = int(input("How many rounds?"))
+    run_program_n_times(user_attempts)
