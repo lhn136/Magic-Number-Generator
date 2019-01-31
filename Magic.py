21fcc064f84d0052a647ddf3993cc7d26404f262
lhn136 <32319480+lhn136@users.noreply.github.com>
2019-01-30 22:53:16 -0800

    First commit

---
+++ Magic.py
@@ -0,0 +1,9 @@
+# Write code clearer practice
+
+import random
+
+# Generate 2 random number in a list
+magic_number = [random.randomint(0,9),random.randint(0,9)]
+
+def user_input_conditional_check():
+    
