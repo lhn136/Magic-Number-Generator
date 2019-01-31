9ca5ef203a629135927333cedaab7119f2daa276
lhn136 <32319480+lhn136@users.noreply.github.com>
2019-01-30 23:07:40 -0800

    Update Magic.py

--- Magic.py
+++ Magic.py
@@ -3,14 +3,15 @@
 import random
 
 # Generate 2 random number in a list
-magic_numbers = [random.randomint(0,9),random.randint(0,9)]
+magic_numbers = [random.randint(0,9),random.randint(0,9)]
 
 def user_input_conditional_check():
     user_number = int(input("Enter a number between 0 and 9: "))
+    # print(magic_numbers[0], "  " ,magic_numbers[1]);
     if user_number in magic_numbers:
-        return "You got the number right!"
+        print ("You got the number right!")
     if user_number not in magic_numbers:
-        return "You didn't quite get it."
+        print( "You didn't quite get it.")
 
 def run_program_n_times(chances):
     for attempt in range(chances):
@@ -19,4 +20,4 @@ def run_program_n_times(chances):
 
 if __name__ == '__main__':
     user_attempts = int(input("How many rounds?"))
-    run_program_n_times(user_attempts)
+    print(run_program_n_times(user_attempts))
